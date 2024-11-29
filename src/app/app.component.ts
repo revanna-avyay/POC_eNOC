6eac2d9362d390ec6939423cb359119ac866ba4c
santoshmohan04 <santosh_tagore04@rediffmail.com>
2024-11-29 10:50:58 +0530

    Layout changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,82 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FileUploadComponent } from './components/file-upload/file-upload.component';
-import { ExpandCollapseComponent } from './components/expand-collapse/expand-collapse.component';
-import { TypeAheadComponent } from './components/type-ahead/type-ahead.component';
-import { SearchComponent } from './components/search/search.component';
-import { fileFormats } from './data/states';
-import { NotificationComponent } from './components/notification/notification.component';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { LangTranslateComponent } from "./components/lang-translate/lang-translate.component";
-import { CommonAlertComponent } from './common/common-alert/common-alert.component';
-import { CommonCardComponent } from './common/common-card/common-card.component';
-import { CommonModule } from '@angular/common';
-import { TableService } from './services/table.service';
-import { CommonTableComponent } from './common/table/common-table/common-table.component';
-import { InformationLinksComponent } from './components/information-links/information-links.component';
-import { PdfComponent } from './components/pdf/pdf.component';
-import { StatusIndicatorComponent } from './components/status-indicator/status-indicator.component';
-import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
-import { ApplicationStatusComponent } from './components/application-status/application-status.component';
-import { MeetingsComponent } from './components/meetings/meetings.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
-const data = [
-  {
-    id: 1,
-    title: "5",
-    subtitle: "Comments",
-  },
-  {
-    id: 2,
-    title: "4",
-    subtitle: "Due To Revalidation",
-  },
-  {
-    id: 3,
-    title: "6",
-    subtitle: "Fines",
-  },
-  {
-    id: 4,
-    title: "9",
-    subtitle: "Payments",
-  },
-  {
-    id: 5,
-    title: "6",
-    subtitle: "Fines",
-  }
-]
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FileUploadComponent, PdfComponent, InformationLinksComponent, ExpandCollapseComponent, TypeAheadComponent, SearchComponent, NotificationComponent, NgbNavModule,
-    LangTranslateComponent, ApplicationStatusComponent, BookmarksComponent, StatusIndicatorComponent, MeetingsComponent,CommonTableComponent,CommonCardComponent,CommonAlertComponent,CommonModule],
-  providers: [TableService],
+  imports: [RouterOutlet],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  title = 'enoc';
-  file_formats = fileFormats;
-  active = "file-upload";
-  cardData: any[] = []
-  customTableData: [] = []
-
-  custTableData = inject(TableService)
-
-  onFilesChanged(files: File[]): void {
-    console.log('Uploaded files:', files);
-  }
-
-  ngOnInit(): void {
-    this.cardData = data
-    this.getCustomTableData()
-  }
-
-  getCustomTableData() {
-    this.custTableData.getTableData().subscribe((res) => {
-      this.customTableData = res
-      console.log(res)
-    })
-  }
+export class AppComponent {
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserhomeComponent } from './components/userhome/userhome.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LayoutComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'userhome', component: UserhomeComponent},
+];
